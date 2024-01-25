7cd159656440e91abcdf7cd98f63f87b1c87eb34
Camila <alvescamila87@gmail.com>
2024-01-25 17:13:06 -0300

    create repository to practicing how to use an API rest template

---
+++ src/main/java/com/camila/demoapiresttemplate/DemoApiRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.camila.demoapiresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camila/demoapiresttemplate/DemoApiRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camila.demoapiresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
